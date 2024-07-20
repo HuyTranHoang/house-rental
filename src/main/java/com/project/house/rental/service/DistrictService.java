82eeb5448d09cfcdcf14ef0485d62ec49b0e25d1
Huy Trần <nhokpio95@gmail.com>
2024-07-20 22:50:26 +0700

    Revert "CRUD-District and Data.sql"

--- src/main/java/com/project/house/rental/service/DistrictService.java
+++
@@ -1,8 +0,0 @@
-package com.project.house.rental.service;
-
-import com.project.house.rental.dto.DistrictDto;
-import com.project.house.rental.entity.District;
-
-
-public interface DistrictService extends GenericService<District, DistrictDto> {
-}
