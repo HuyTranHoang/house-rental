f611e50db9216b4a46928f69eff669c908dbaaf9
Huy Trần <nhokpio95@gmail.com>
2024-07-30 22:00:48 +0700

    Add filter, sort, pagination for Review

--- src/main/java/com/project/house/rental/dto/ReviewDto.java
+++ src/main/java/com/project/house/rental/dto/ReviewDto.java
@@ -1,5 +1,8 @@
 package com.project.house.rental.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -13,8 +16,11 @@ public class ReviewDto {
 
     long id;
 
+    @Min(value = 1, message = "Đánh giá phải từ 1 đến 5 sao")
+    @Max(value = 5, message = "Đánh giá phải từ 1 đến 5 sao")
     int rating;
 
+    @Size(min = 16, max = 500, message = "Nhận xét phải từ 16 đến 500 ký tự")
     String comment;
 
     long propertyId;
--- src/main/java/com/project/house/rental/service/ReviewService.java
+++ src/main/java/com/project/house/rental/service/ReviewService.java
@@ -1,11 +1,16 @@
 package com.project.house.rental.service;
 
 import com.project.house.rental.dto.ReviewDto;
+import com.project.house.rental.dto.params.ReviewParams;
 import com.project.house.rental.entity.Review;
 import com.project.house.rental.exception.CustomRuntimeException;
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.Map;
+
 public interface ReviewService extends GenericService<Review, ReviewDto> {
+
+    Map<String, Object> getAllReviewsWithParams(ReviewParams reviewParams);
 
     ReviewDto create(ReviewDto reviewDto, HttpServletRequest request) throws CustomRuntimeException;
 
