cdbdda96ba1e5b782b00ef574fbcfd7ed3ded252
levuong14112000 <levuong2000gl@gmail.com>
2024-08-06 20:51:59 +0700

    seed data property

---
+++ src/main/resources/db/migration/V105__seed_data_property_1.sql
@@ -0,0 +1,168 @@
+INSERT IGNORE INTO properties(id, title, description, price, location, area, num_rooms, status, user_id, city_id,
+                              district_id, room_type_id, is_deleted, created_at, updated_at)
+VALUES (4, 'Phòng studio,gác ban công mới full nội thất sát lotte quận 7',
+        'GIẢM GIÁ ƯU ĐÃI KHUYẾN MÃI
+✅ Khai trương Căn hộ siêu HOT ngay đường Lâm Văn Bền, phường Tân Kiểng, Quận 7..,
+👉 gần Lotte Mart -
+
+✅ Khai trương Căn hộ siêu HOT ngay đường Lâm Văn Bền, phường Tân Kiểng, Quận 7..,
+👉 gần Lotte Mart - kế bên Sunrise City, 5p đến Phú Mỹ Hưng, ĐH RMIT - ĐH Tôn Đức Thắng, cầu Kênh Tẻ Quận 7
+⭐Gần trung tâm mua sắm BigC, Crescent Mall, Lotte Mart, gần chợ Tân Quy, siêu thị bách hóa xanh...,',
+        4700000, 'Lâm văn Bền, Phường Bình Thuận, Quận 7, TPHCM', 15, 1, 'RESOLVED', 1, 1, 1, 1, 0, random_date_within_14_days(), NOW()),
+
+       (5, 'Căn hộ mới xây giá siêu rẻ full nội thất gác cao siêu đẹp sát q4',
+        'khai trương Căn hộ full nội thât đường nguyễn văn linh  -🏡
+
+🏵Gía siêu rẻ gác cao
+
+🌻Nội thất bao gồm máy lạnh tủ lanh, máy giặc, máy nước nóng kệ bếp gối mền, chăn ra
+
+❌Dịch vụ tòa nhà
+- Free giặc sấy 2 lần 1 tuần
+- Có máy lọc nước mỗi tầng
+- Có nhân viên dọn vệ sinh
+- điện 2k9/1kwh
+- Nước 18k/khối',
+        5000000, 'Nguyễn Văn Linh, Phường Tân Thuận Tây, Quận 7, TPHCM', 30, 1, 'RESOLVED', 1, 1, 7, 1, 0, random_date_within_14_days(), NOW()),
+
+      (6, 'Phòng trọ riêng biệt, giờ giấc tự do, khu ở an ninh, thoáng mát',
+        'CHO THUÊ PHÒNG TRỌ RIÊNG BIỆT
+- phòng riêng biệt, có chỗ nấu ăn và vệ sinh trong phòng
+- giờ giấc tự do
+- Khu vực an ninh, thoáng mát
+- Ra vào cửa vân tay, camara 24/24
+- Phòng không nuôi thú cưng
+- phòng kéo bạn bè về, không tụ ập ăn uống, hát hò
+👉 Giá: 1tr7, ở 1 người, phòng không gác
+👉 Giá: 2tr2, ở 1-2 người, có gác, phòng như hình
+Và phòng 2 triệu và 2tr4 đều có gác',
+        2400000, 'Chuyên Dùng, Phường Phú Mỹ, Quận 7, TPHCM', 15, 1, 'RESOLVED', 1, 1, 10, 1, 0, random_date_within_14_days(), NOW()),
+
+      (7, 'Ngay Tôn đức thắng_Nguyễn hữu cảnh - Nhà mới có ban công',
+        'Cho thuê căn hộ mới setup nội thất,
+full mới 100% có cửa sổ và ban công
+y bản thiết kế đến 90%
+xem phòng trực tiếp liên hệ 0937317989 - mr Quân chính chủ thương lượng
+ko giống hình hoàn tiền cọc.
+Có hầm xe..
+Gía studio nhỏ: 6,500,000đ cửa sổ - 7,500,000đ ban công
+1PN : 9,000,000đ - 11,000,000đ
+2PN - 2WC: 13,000,000đ',
+        6500000, 'Ngô Tất Tố, Phường 22, Quận Bình Thạnh, TPHCM', 25, 1, 'RESOLVED', 1, 1, 13, 2, 0, random_date_within_14_days(), NOW()),
+
+       (8, 'CHDV. Zá 1PN, Chỉ 6tr5 7,5tr 8,5tr 10Tr',
+        'ĐC: Lương Đình Của QUẬN 2 Thuận Tiện Duy Chuyển Qua 1,3,7,bình Thạnh - CÓ BAN CÔNG -
+        - Phòng được trang bị Full nội thất cao cấp A-Z
+        - Được nuôi Thú Cưng
+        - Giờ giấc tự do, không chung chủ,camera 24/24
+        - Có nhà xe Rộng
+        - Không chung chủ giờ giất tự do
+        - Dọn Phòng Tuần Lần
+        Ngoài ra mình còn rất nhiều dạng phòng
+        - Studio full nội thất: 5.000.000 - 9.000.000
+        - Gác full nội thất: 5.000.000 - 8.000.000
+        - 1PN: 7.500.000 - 15.000.000
+        - 2PN: 10.000.000 - 20.000.000',
+        6500000, 'Lương Định Của, Phường Bình Khánh, Quận 2 (TP. Thủ Đức), TPHCM', 50, 1, 'RESOLVED', 1, 1, 2, 2, 0, random_date_within_14_days(), NOW()),
+
+       (9, 'Mới Khai Trương Ban Công Hiện Đại Quận 2',
+        'CHDV. Zá 1PN, Chỉ 6tr5 7,5tr 8,5tr 10Tr
+        ĐC: Lương Đình Của QUẬN 2 Thuận Tiện Duy Chuyển Qua 1,3,7,bình Thạnh - CÓ BAN CÔNG -
+        - Phòng được trang bị Full nội thất cao cấp A-Z
+        - Được nuôi Thú Cưng
+        - Giờ giấc tự do, không chung chủ,camera 24/24
+        - Có nhà xe Rộng
+        - Không chung chủ giờ giất tự do
+        - Dọn Phòng Tuần Lần
+        Ngoài ra mình còn rất nhiều dạng phòng',
+        6500000, 'Lương Định Của, Phường Bình Khánh, Quận 2 (TP. Thủ Đức), TPHCM', 50, 1, 'RESOLVED', 1, 1, 2, 2, 0, random_date_within_14_days(), NOW()),
+
+      (10, 'Cho Thuê Căn Hộ Full Nội Thất Cao Cấp Máy Giặt Riêng Từng Phòng Quận 7',
+        'Siêu Phẩm Mới Keng🤩
+        🌻Địa chỉ: KDC Trung Sơn_Himlam_quận 7🌻
+        💰 Giá Từ 9 --> 12tr Theo Dạng Phòng
+        ( Studio - 1Phòng Ngủ - 2 Phòng Ngủ )
+        ✔ Studio full nội thất cao cấp
+        ✔️Giờ giấc tự do không chung chủ
+        ✔️Có hầm xe rộng rãi',
+        8500000, 'Hoàng Trọng Mậu, Phường Tân Hưng, Quận 7, TPHCM', 35, 1, 'RESOLVED', 1, 1, 2, 7, 0, random_date_within_14_days(), NOW()),
+
+       (11, 'Cho thuê căn hộ 2PN đẹp lung linh tại Vinhomes Grand Park - Origami',
+        'Diện tích: 70m2 (2PN, 2WC)
+        🏢 Tòa S10.06 - Nội thất đầy đủ
+        💰 Giá thuê: 9 triệu/tháng (cọc 2 tháng)
+        **☎️ Liên hệ ngay: 0707.052.200**
+        Sống trọn vẹn tiện nghi, tận hưởng cuộc sống đẳng cấp tại Vinhomes Grand Park!',
+        9000000, 'Phước Thiện, Phường Long Thạnh Mỹ, Quận 9 (TP. Thủ Đức), TPHCM', 70, 2,'RESOLVED', 1, 1, 2, 7, 0, random_date_within_14_days(), NOW());
+
+INSERT IGNORE INTO property_amenities(property_id, amenity_id)
+VALUES
+       (4, 1),
+       (4, 2),
+       (4, 3),
+       (4, 4),
+       (4 ,5),
+       (5, 1),
+       (5, 2),
+       (5, 3),
+       (5, 4),
+       (5 ,5),
+       (6, 1),
+       (6, 2),
+       (6, 3),
+       (6, 4),
+       (6 ,5),
+       (7, 1),
+       (7, 2),
+       (7, 3),
+       (7, 4),
+       (7 ,5),
+       (8, 1),
+       (8, 2),
+       (8, 3),
+       (8, 4),
+       (8 ,5),
+       (9, 1),
+       (9, 2),
+       (9, 3),
+       (9, 4),
+       (9 ,5),
+       (10, 1),
+       (10, 2),
+       (10, 3),
+       (10, 4),
+       (10 ,5),
+       (11, 1),
+       (11, 2),
+       (11, 3),
+       (11, 4),
+       (11 ,5);
+
+
+INSERT IGNORE INTO property_images(property_id, image_url)
+VALUES (11, 'https://cloud.mogi.vn/images/2024/04/07/099/c1e354477b454950b47a8468cc10a030.jpg'),
+       (11, 'https://cloud.mogi.vn/images/2024/04/07/090/c3b9e607bf4544289af044e441c4c496.jpg'),
+       (11, 'https://cloud.mogi.vn/images/2024/04/07/091/1826dde3bfe34b07924609b3c27273b2.jpg'),
+       (11, 'https://cloud.mogi.vn/images/2024/04/07/092/d2b86e88620047d695e210c598fdd90e.jpg'),
+       (10, 'https://cloud.mogi.vn/images/2023/10/21/599/b2da48285d0f431b99def8e116d39f3e.jpg'),
+       (10, 'https://cloud.mogi.vn/images/2023/10/21/599/50dcaaf83aed44828c8803351ee8978f.jpg'),
+       (10, 'https://cloud.mogi.vn/images/2023/10/21/590/dcd025a6a7964dba860ca840a5d23704.jpg'),
+       (9, 'https://cloud.mogi.vn/images/2024/08/01/213/7e2a6268f0b4438abb0f034640c21607.jpg'),
+       (9, 'https://cloud.mogi.vn/images/2024/08/01/212/dacd7232d9334d77ae251ef3ba94aa84.jpg'),
+       (9, 'https://cloud.mogi.vn/images/2024/08/01/215/008bede5a8ec48c5bec37baf8193794b.jpg'),
+       (8, 'https://cloud.mogi.vn/images/2021/05/17/497/c499b9294de74fb6bedb5af57ba83e2f.jpg'),
+       (8, 'https://cloud.mogi.vn/images/2021/05/17/494/8556b46603b6415e9bf821dfa90fe402.jpg'),
+       (8, 'https://cloud.mogi.vn/images/2021/06/22/256/d81bf04a210d461881dcff5794f8852e.jpg'),
+       (7, 'https://cloud.mogi.vn/images/2024/06/11/214/f63b3d90ff2144b5add2f531f17753f2.jpg'),
+       (7, 'https://cloud.mogi.vn/images/2024/06/11/215/5af245c54dd248739e18a2cc27344f04.jpg'),
+       (7, 'https://cloud.mogi.vn/images/2024/06/11/216/081cd9dd60414d06a25ff3afe6760b1c.jpg'),
+       (6, 'https://cloud.mogi.vn/images/2024/06/11/216/081cd9dd60414d06a25ff3afe6760b1c.jpg'),
+       (6, 'https://cloud.mogi.vn/images/2024/06/11/217/21990d16e21347b98f2a82e5934a725f.jpg'),
+       (6, 'https://cloud.mogi.vn/images/2024/06/11/219/cd31b7c9101b44deb592e9c97b0ec2e9.jpg'),
+       (5, 'https://cloud.mogi.vn/images/2023/09/04/596/99de795aad574d8ca6a33f58a0098aea.jpg'),
+       (5, 'https://cloud.mogi.vn/images/2023/09/04/597/1d08f6a9b1864934a0ea3c224db9117b.jpg'),
+       (5, 'https://cloud.mogi.vn/images/2023/09/04/598/353e66e55b904638a1d37646546e882d.jpg'),
+       (4, 'https://cloud.mogi.vn/images/2024/06/11/584/8dc7c5daf53f4f9aa27b5ad7a20ebad6.jpg'),
+       (4, 'https://cloud.mogi.vn/images/2024/06/11/585/9831629ab0004a14ad0ff111a74a0e34.jpg'),
+       (4, 'https://cloud.mogi.vn/images/2024/06/11/586/146912a6c9ae4f5d8ca378bdf1b3972d.jpg')
+    ;
