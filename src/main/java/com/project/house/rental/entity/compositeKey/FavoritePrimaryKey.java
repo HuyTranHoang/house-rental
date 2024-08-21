d68ed93ed07a171f8ae755a2cd5a7dfbb9e420ae
Huy Trần <nhokpio95@gmail.com>
2024-08-21 15:39:44 +0700

    Cap nhat equals hashcode cho composite key

--- src/main/java/com/project/house/rental/entity/compositeKey/FavoritePrimaryKey.java
+++ src/main/java/com/project/house/rental/entity/compositeKey/FavoritePrimaryKey.java
@@ -16,4 +16,20 @@ public class FavoritePrimaryKey {
     long userId;
 
     long propertyId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FavoritePrimaryKey that = (FavoritePrimaryKey) o;
+        return userId == that.userId && propertyId == that.propertyId;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Long.hashCode(userId);
+        result = 31 * result + Long.hashCode(propertyId);
+        return result;
+    }
 }
