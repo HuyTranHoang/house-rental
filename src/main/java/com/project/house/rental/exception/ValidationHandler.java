9b5ae264573af382a12bb2f2de87bdf0cb9c3a0f
Huy Trần <nhokpio95@gmail.com>
2024-08-29 21:33:45 +0700

    Fix NoResultException bị sai status

--- src/main/java/com/project/house/rental/exception/ValidationHandler.java
+++ src/main/java/com/project/house/rental/exception/ValidationHandler.java
@@ -1,24 +1,18 @@
 package com.project.house.rental.exception;
 
 import com.project.house.rental.common.HttpResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
-
 @ControllerAdvice
 public class ValidationHandler extends ResponseEntityExceptionHandler {
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
@@ -30,14 +24,4 @@ public class ValidationHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<HttpResponse> handleAllException(Exception e) {
-        logger.error(e.getMessage());
-        return createResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-    }
-
-    private ResponseEntity<HttpResponse> createResponseEntity(HttpStatus status, String message) {
-        HttpResponse httpResponse = new HttpResponse(status.value(), status, status.getReasonPhrase(), message);
-        return new ResponseEntity<>(httpResponse, status);
-    }
 }
