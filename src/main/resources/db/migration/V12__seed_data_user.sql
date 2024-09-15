9e8fd33a9b25b1a020ed4712f427650d9876f677
Huy Trần <nhokpio95@gmail.com>
2024-09-16 02:53:04 +0700

    Cập nhật env cho database mới.

--- src/main/resources/db/migration/V12__seed_data_user.sql
+++ src/main/resources/db/migration/V12__seed_data_user.sql
@@ -1,10 +1,10 @@
 INSERT INTO users (id, username, password, email, phone_number, first_name, last_name, avatar_url, is_active,
                    is_non_locked, is_deleted, created_at, updated_at)
 VALUES (2, 'user', '$2a$10$aPFK5XoRGGEu/zMu/P11z.yO/QxtJyVUQD4euihW.A428tTVHt3je', 'user@gmail.com', '0123456789',
-        'User', 'Test',
+        'User', 'Mogu',
         'https://robohash.org/user?set=set4', true, true, false, random_date_within_14_days(), now()),
        (1, 'admin', '$2a$10$aPFK5XoRGGEu/zMu/P11z.yO/QxtJyVUQD4euihW.A428tTVHt3je', 'admin@gmail.com', '0123456789',
-        'Admin', 'Test',
+        'Admin', 'Mogu',
         'https://robohash.org/admin?set=set4', true, true, false, random_date_within_14_days(), now())
 ON CONFLICT (id) DO NOTHING;
 
