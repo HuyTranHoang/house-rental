9288d120216ad904a662ec65ae026045a93912f4
Huy Trần <nhokpio95@gmail.com>
2024-09-27 04:50:35 +0700

    Cập nhật controlller cho CommentReport

---
+++ src/main/java/com/project/house/rental/controller/CommentReportController.java
@@ -0,0 +1,63 @@
+package com.project.house.rental.controller;
+
+import com.project.house.rental.dto.CommentReportDto;
+import com.project.house.rental.dto.ReportDto;
+import com.project.house.rental.dto.params.CommentReportParams;
+import com.project.house.rental.dto.params.ReportParams;
+import com.project.house.rental.service.CommentReportService;
+import com.project.house.rental.service.ReportService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/reports")
+public class CommentReportController {
+    private final CommentReportService commentReportService;
+
+    public CommentReportController(CommentReportService commentReportService) {
+        this.commentReportService = commentReportService;
+    }
+
+    @GetMapping({"/", ""})
+    public ResponseEntity<Map<String, Object>> getAllReports(@ModelAttribute CommentReportParams reportParams) {
+        Map<String, Object> reportsWithPagination = commentReportService.getAllCommentReportsWithParams(reportParams);
+        return ResponseEntity.ok(reportsWithPagination);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CommentReportDto>> getAllReports(@RequestParam(required = false) String username) {
+        List<CommentReportDto> reportDtos = commentReportService.getAllCommentReports(username);
+        return ResponseEntity.ok(reportDtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CommentReportDto> getDistrictById(@PathVariable long id){
+        CommentReportDto reportDto = commentReportService.getCommentReportById(id);
+        return ResponseEntity.ok(reportDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentReportDto> createReport(@Valid @RequestBody CommentReportDto reportDto, HttpServletRequest request) {
+        CommentReportDto report = commentReportService.createCommentReport(reportDto, request);
+        return  ResponseEntity.ok(report);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReport(@PathVariable Long id) {
+        commentReportService.deleteCommentReportById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateReportStatus(@PathVariable Long id,@RequestParam String status) {
+        commentReportService.updateCommentReportStatus(id, status);
+        return ResponseEntity.noContent().build();
+    }
+
+}
